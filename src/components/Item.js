75158a3e94caf7ec62382e88fb6a54160afd2aec
Miguel Vicente Robles Mclean <miguel.robles@toolfactory.net>
2023-02-08 13:16:35 +0100

    Double Click Added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import '../components-styles/Item.css';
 
 function Item({id, text, selected, selectItem}){
+
+  const [visible, setVisible] = useState(true);
+
+  const handleClick = (e) => {
+    switch (e.detail) {
+      case 1:
+        selectItem(id)
+        break;
+      case 2:
+        removeElement()
+        break;
+      default:
+        break;
+      
+    }
+  };
+
+  const removeElement = () => {
+    setVisible((prev) => !prev);
+  }
+
   return (
-    <div 
-      className={selected ? 'item-container selected' : 'item-container'}
-      onClick={() => selectItem(id)} >
-      <div 
-        className='item-text'>
-        {text}
-      </div>
+    <div>
+      {visible && (
+        <div 
+          className={selected ? 'item-container selected' : 'item-container'}
+          onClick={handleClick}>
+          <div 
+           className='item-text'>
+           {text}
+          </div>
+        </div>
+        )}
     </div>
   );
 }
